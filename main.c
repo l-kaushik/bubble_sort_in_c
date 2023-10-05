b4bb97731352b646374d41e627db35ccbceb4993
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-05 20:22:53 +0530

    Added a function to check if array is already sorted then stop the iteration of passes

--- main.c
+++ main.c
@@ -8,20 +8,27 @@ void printArray(int A[], int n){
 }
 
 void bubbleSort(int A[], int n){
+    int temp, isSorted;
     for(int i = 0; i<n-1; i++){ // loop for passes
-        for(int j = 0; j<n-i-1; j++){
+        isSorted = 1;
+        for(int j = 0; j<n-i-1; j++){ // for comparison in each pass
             if(A[j] > A[j+1]){
-                int temp = A[j];
+                temp = A[j];
                 A[j] = A[j+1];
                 A[j+1] = temp;
+                isSorted = 0; 
             }
+        }
+        if(isSorted){  // checking if array is already sorted or not
+            return; 
         }
     }
 }
 
 int main()
 {
-    int A[] = {12, 54, 65, 7, 23, 9, 2};
+    // int A[] = {12, 54, 65, 7, 23, 9, 2};
+    int A[] = {1,2,3,4,5,6,7};
     int n = 7;
     printArray(A, n);
     bubbleSort(A,n);
