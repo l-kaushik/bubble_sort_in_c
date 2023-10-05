6590decb33fb4a069aa4f3bbef618e793e95dc03
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-05 20:02:58 +0530

    Added printArray function to print the array

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 
+void printArray(int A[], int n){
+    for(int i = 0; i<n; i++){
+        printf("%d ",A[i]);
+    }
+}
+
 int main()
 {
-    printf("Hello world");
+    int A[] = {12, 54, 65, 7, 23, 9};
+    int n = 6;
+    printArray(A, n);
+
     return 0;
 }
