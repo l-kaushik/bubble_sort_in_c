e5702354b80d193f3aeb74984bffe5a1bf68e425
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-05 20:14:18 +0530

    Added the sorting function

--- main.c
+++ main.c
@@ -4,13 +4,27 @@ void printArray(int A[], int n){
     for(int i = 0; i<n; i++){
         printf("%d ",A[i]);
     }
+    printf("\n");
+}
+
+void bubbleSort(int A[], int n){
+    for(int i = 0; i<n-1; i++){ // loop for passes
+        for(int j = 0; j<n-i-1; j++){
+            if(A[j] > A[j+1]){
+                int temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+            }
+        }
+    }
 }
 
 int main()
 {
-    int A[] = {12, 54, 65, 7, 23, 9};
-    int n = 6;
+    int A[] = {12, 54, 65, 7, 23, 9, 2};
+    int n = 7;
     printArray(A, n);
-
+    bubbleSort(A,n);
+    printArray(A, n);
     return 0;
 }
