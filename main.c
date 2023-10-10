ed404fd9bf72170c72cedf07def9307956bf1c34
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-10 14:22:02 +0530

    added printing array function

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
+void printArray(int A[], int n){
+    for(int i = 0; i<n; i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
-    printf("Hello world");
+    int a[] = {8,0,7,1,3};
+    printArray(a, 5);
     return 0;
 }
