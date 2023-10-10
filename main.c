9a99a6c04441a7d61426af5315da21c4e71d49ff
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-10 14:55:03 +0530

    Added function of sorting

--- main.c
+++ main.c
@@ -7,9 +7,23 @@ void printArray(int A[], int n){
     printf("\n");
 }
 
+void selectionSort(int a[], int size){
+    for(int i = 0; i<size; i++){
+        int min = i;
+        for(int j = i+1; j<size; j++){
+            if(a[min] > a[j]){min = j;}
+        }
+        int temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+}
+
 int main()
 {
-    int a[] = {8,0,7,1,3};
-    printArray(a, 5);
+    int a[] = {8,0,7,1,3,-2};
+    printArray(a, 6);
+    selectionSort(a,6);
+    printArray(a, 6);
     return 0;
 }
