c2c65d8e1133b9462ff17726cb1f1e6c0864695f
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-18 15:59:29 +0530

    added merge function

--- mergeSort.c
+++ mergeSort.c
@@ -8,6 +8,42 @@ void printArray(int A[], int n){
     printf("\n");
 }
 
+void merge(int a[], int low, int mid, int high){
+    int i,j,k;
+    int b[high];
+    i = low;
+    j = mid+1;
+    k = low;
+
+    // copying smaller elements to array b
+    while(i <= mid && j <= high){
+        if(a[i] < a[j]){
+            b[k] = a[i];
+            i++; k++;
+        }
+        else{
+            b[k] = a[j];
+            j++; k++;
+        }
+    }
+
+    // copying rest of the elements to array b 
+    while(i <= mid){
+        b[k] = a[i];
+        k++; i++;
+    }
+
+    while(j <= high){
+        b[k] = a[j];
+        j++; k++;
+    }
+
+    // moving to array a
+    for(i = low; i<= high; i++){
+        a[i] = b[i];
+    }
+}
+
 void mergeSort(int a[], int low, int high){
     int mid;
     if(low < high){
@@ -23,5 +59,7 @@ int main()
 {
     int a[] = {8,0,7,1,3,-2};
     printArray(a, 6);
+    mergeSort(a,0,6);
+    printArray(a,6);
     return 0;
 }
