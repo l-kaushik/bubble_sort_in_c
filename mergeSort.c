513b4362406659c548abf46cf62a28de32a2abea
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-18 15:32:58 +0530

    created recursive mergeSort function, to break array

--- mergeSort.c
+++ mergeSort.c
@@ -8,6 +8,17 @@ void printArray(int A[], int n){
     printf("\n");
 }
 
+void mergeSort(int a[], int low, int high){
+    int mid;
+    if(low < high){
+        mid = (low + high)/2;
+
+        mergeSort(a, low, mid);
+        mergeSort(a, mid+1, high);
+        merge(a,low,mid, high);
+    }
+}
+
 int main()
 {
     int a[] = {8,0,7,1,3,-2};
