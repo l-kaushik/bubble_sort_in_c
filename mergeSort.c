fe100438be8583d44ea0555ea9ca5eee1733b981
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-18 15:30:12 +0530

    added print array function

--- mergeSort.c
+++ mergeSort.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void printArray(int A[], int n){
+    for(int i = 0; i<n; i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
-    printf("Hello World");
+    int a[] = {8,0,7,1,3,-2};
+    printArray(a, 6);
     return 0;
 }
