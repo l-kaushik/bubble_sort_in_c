a162e77b59c7387d221cb8eb5a9b617b5e8813e5
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-20 12:33:16 +0530

    added count sort functin

--- countSort.c
+++ countSort.c
@@ -8,9 +8,42 @@ void printArray(int a[], int n){
     printf("\n");
 }
 
+void countSort(int a[], int size){
+    // finding max element
+    int max = a[0];
+    for(int i = 1; i<size; i++){
+        if(a[i] > max){
+            max = a[i];
+        }
+    }
+    
+    // creating array of length = max + 1
+    max++;
+    int *countArr = (int *)calloc(max, sizeof(int));
+    for(int i = 0; i<max; i++){
+        for(int j = 0; j<size; j++){
+            if(a[j] == i){
+                countArr[i]++;
+            }
+        }
+    }
+    int j = 0;
+    for(int i = 0; i <size;){
+        while(countArr[j] > 0){
+            a[i] = j;
+            i++;
+            countArr[j]--;
+        }
+        j++;
+    }
+}
+
 int main()
 {
-    int a[] = {1,2,3,4,5,6,7};
+    int a[] = {3,4,9,9,2,3,4,1};
     int size = sizeof(a)/sizeof(a[0]);
+    printArray(a, size);
+    countSort(a,size);
+    printArray(a,size);
     return 0;
 }
